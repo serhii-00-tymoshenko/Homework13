595e66836e4b6d74ef0be407381b0986284dbb16
Serhii Tymoshenko <serhii.tymoshenko.ua@gmail.com>
2023-11-23 09:01:13 +0100

    Fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,16 @@
-import java.util.Properties
 
 fun main(args: Array<String>) {
     val person = Person("John")
-    println(person.showGreeting())
+    person.showGreeting()
 
     val person1 = Person()
-    println(person1.showGreeting())
+    person1.showGreeting()
 
     println("    hello   ".uppercaseAndTrim())
 
-    println(person.sleep())
-    println(person.eat())
-    println(person.breath())
+    person.sleep()
+    person.eat()
+    person.breath()
 }
 
 private fun String.uppercaseAndTrim(): String {
